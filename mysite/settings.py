ed2b751d512d7b4d937f42fa1d52f17853553c96
milaMeowMeow <milana.stolyarova.00@bk.ru>
2024-05-30 22:52:52 +0300

    My Django Girls app, first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
+from pathlib import Path,os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,14 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m49er#s$kz@%!toda!g=2#hf!)b1^c%ozsym$pb1m@)+k^jy0q'
+SECRET_KEY = 'django-insecure-&cjqm-yemms$bvgsru#1zgm2%xl00-w(tmkpr534t$6v(pmdt)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
-from pathlib import Path
-import os
 
 
 # Application definition
